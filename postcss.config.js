cef2a40426f7d690095727be98df3f704f83be01
SaiOsore <sai.osore@gmail.com>
2020-05-17 00:15:36 +0300

    mqpacker error

--- postcss.config.js
+++ postcss.config.js
@@ -7,7 +7,9 @@
 module.exports = {
   plugins: [
     require('autoprefixer'),
-    require('css-mqpacker'),
+    // require('css-mqpacker')({
+    //   sort: true
+    // }),
     require('cssnano')({
       preset: [
         'default', {
