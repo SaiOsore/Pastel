9195bef19c9d23cb1b5de61dff56d8a49169deec
SaiOsore <sai.osore@gmail.com>
2020-05-27 13:07:42 +0300

    touch event for home slider

--- src/js/components/HomeSlider.js
+++ src/js/components/HomeSlider.js
@@ -1,53 +1,67 @@
 const homeContainer = document.querySelector('.home .container');
-const homeSections = document.querySelectorAll('.home__section');
 
-let length = 0;
-let prevLength;
+if(homeContainer) {
 
-const prevSlide = (e) => {
-  if(length === 0) {
-    length = homeSections.length - 1;
-    prevLength = 0;
-  } else {
-    length--;
-    prevLength = length + 1;
+  const homeSections = document.querySelectorAll('.home__section');
+
+  const homeContainerHeight = homeSections[0].clientHeight;
+  let length = 0;
+  let prevLength;
+
+  const prevSlide = (e) => {
+    if(length === 0) {
+      length = homeSections.length - 1;
+      prevLength = 0;
+    } else {
+      length--;
+      prevLength = length + 1;
+    }
+    homeSections[prevLength].classList.remove('active');
+    homeSections[length].classList.add('active');
   }
-  homeSections[prevLength].classList.remove('active');
-  homeSections[length].classList.add('active');
-}
 
-const nextSlide = (e) => {
-  if(length === homeSections.length - 1) {
-    length = 0;
-    prevLength = homeSections.length - 1;
-  } else {
-    length++;
-    prevLength = length - 1;
+  const nextSlide = (e) => {
+    if(length === homeSections.length - 1) {
+      length = 0;
+      prevLength = homeSections.length - 1;
+    } else {
+      length++;
+      prevLength = length - 1;
+    }
+    homeSections[prevLength].classList.remove('active');
+    homeSections[length].classList.add('active');
   }
-  homeSections[prevLength].classList.remove('active');
-  homeSections[length].classList.add('active');
-}
 
-homeContainer.addEventListener('wheel', (e) => {
-  let moved = e.wheelDelta;
-  if(moved > 0) {
-    prevSlide();
-  } else if(moved < 0) {
-    nextSlide();
-  }
-});
+  homeContainer.addEventListener('wheel', (e) => {
+    let moved = e.wheelDelta;
+    if(moved > 0) {
+      prevSlide();
+    } else if(moved < 0) {
+      nextSlide();
+    }
+  });
 
-window.addEventListener('keydown', (e) => {
-  if(e.keyCode == '38' || e.keyCode == '37') {
-    prevSlide();
-  }
-  if(e.keyCode == '40' || e.keyCode == '39') {
-    nextSlide();
-  }
-});
+  window.addEventListener('keydown', (e) => {
+    if(e.keyCode == '38' || e.keyCode == '37') {
+      prevSlide();
+    }
+    if(e.keyCode == '40' || e.keyCode == '39') {
+      nextSlide();
+    }
+  });
 
-const autoplay = () => {
-  setInterval(() => {
-    nextSlide();
-  }, 10000);
+  homeContainer.addEventListener('touchend', (e) => {
+    if(e.changedTouches[0].pageY < homeContainerHeight / 2) {
+      prevSlide();
+    } else {
+      nextSlide();
+    }
+  });
+
+  const autoplay = () => {
+    setInterval(() => {
+      nextSlide();
+    }, 10000);
+  }
+
 }
